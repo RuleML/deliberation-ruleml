931bcdbcc817cac273482df9c6d4a997abb5b597
greenTara <taraathan@gmail.com>
2014-03-20 11:24:40 -0400

    setup schema docs scripts
    Signed-off-by: greenTara <taraathan@gmail.com>

--- bash/web2xsd.sh
+++ bash/web2xsd.sh
@@ -33,4 +33,9 @@ curl -L "http://deliberation.ruleml.org/1.01/relaxng/schema_rnc.php?backbone=x7&
 curl -L "http://deliberation.ruleml.org/1.01/relaxng/schema_rnc.php?backbone=x7&default=x6&termseq=x7&lng=x1&propo=x3c1&implies=x7e&terms=xf10&quant=x0&expr=x0&serial=x0"  > ../relaxng/tmp-web2xsd.rnc 
 ./rnc2xsd.sh ../relaxng/tmp-web2xsd.rnc ../xsd/disdatalogplus_mid_normal.xsd
 
+
+# http://deliberation.ruleml.org/1.01/xsd/naffologeq_normal.xsd
+curl -L "http://deliberation.ruleml.org/1.01/relaxng/schema_rnc.php?backbone=x7&default=x6&termseq=x7&lng=x1&propo=x3c1&implies=x7e&terms=xf10&quant=x0&expr=x0&serial=x0"  > ../relaxng/tmp-web2xsd.rnc 
+./rnc2xsd.sh ../relaxng/tmp-web2xsd.rnc ../xsd/naffologeq_normal.xsd
+
 rm ../relaxng/tmp-web2xsd.rnc
