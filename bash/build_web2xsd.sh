e1e27ed60163947e6ed558c20f7ece6c2da08e3e
greenTara <taraathan@gmail.com>
2014-11-18 10:34:22 -0500

    calling batch script from build script for XSDs

--- bash/build_web2xsd.sh
+++ bash/build_web2xsd.sh
@@ -14,40 +14,21 @@ BASH_HOME=~/Repositories/RuleML/Github/deliberation-ruleml/bash
 RNC4XSD_HOME=${BASH_HOME}/../relaxng/rnc4xsd 
 XSD_HOME=${BASH_HOME}/../xsd 
 # 
-# creates the xsd and xsd_min directory if they don't exist, and clears them, in case they already have contents
-mkdir -p ${XSD_HOME}
-mkdir -p "${XSD_HOME}_min"
-rm ${XSD_HOME}/*.xsd
-rm ${XSD_HOME}_min/*.xsd
 
 ${BASH_HOME}/batch_config2rnc4xsd.sh >> /dev/null 2>&1
 if [ "$?" -ne "0" ]; then
    echo "Configuration of RNC4XSD Failed"
    exit 1
 fi
+
+# Download RNC and simplify, and validate
+${BASH_HOME}/batch_rnc2xsd.sh
+if [ "$?" -ne "0" ]; then
+     echo "Validation Against Design Failed"
+     exit 1
+fi
    
-for f in ${RNC4XSD_HOME}/*.rnc
-do
-  filename=$(basename "$f")
-  extension="${filename##*.}"
-  filenameNE="${filename%.*}"
-  ${BASH_HOME}/rnc2xsd.sh "$f" ${XSD_HOME}/"$filenameNE".xsd
-done
-
-for f in ${RNC4XSD_HOME}_min/*.rnc
-do
-  filename=$(basename "$f")
-  extension="${filename##*.}"
-  filenameNE="${filename%.*}"
-  ${BASH_HOME}/rnc2xsd.sh "$f" ${XSD_HOME}_min/"$filenameNE".xsd
-done
-
-rm ${XSD_HOME}/xml.xsd
-# Apply XSLT transforamtions
-# java  -jar dir/saxon9ee.jar
-# Validate Examples XSD Test Suites
-# @@@
-# Validate Examples in Relax NG Test Suites
+# Validate Examples in XSD Test Suites
 exit
 
 
