0b32b7b9c3003074de21839dbc7b8771468ad7ea
greenTara <taraathan@gmail.com>
2015-06-04 14:46:26 -0400

    adding build of module zip file to script, with result in module directory

--- bash/build_zip.sh
+++ bash/build_zip.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 # dc:rights [ 'Copyright 2015 RuleML Inc. -- Licensed under the RuleML Specification License, Version 1.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://ruleml.org/licensing/RSL1.0-RuleML. Disclaimer: THIS SPECIFICATION IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES, ..., EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. See the License for the specifics governing permissions and limitations under the License.' ]
 # Dependencies
-# build_myng2rnc.sh
-# build_myng2xsd.sh
 shopt -s nullglob
 BASH_HOME=$( cd "$(dirname "$0")" ; pwd -P )/
-
-# Test RNC locally
-${BASH_HOME}build_myng2rnc.sh
-if [ "$?" -ne "0" ]; then
-     echo "Local Testing of RNC Schemas Failed"
-     exit 1
-fi
-# Build XSD locally and test
-${BASH_HOME}build_myng2xsd.sh
-if [ "$?" -ne "0" ]; then
-     echo "Local Testing of XSD Schemas Failed"
-     exit 1
-fi
-# Build zip files
-${BASH_HOME}build_zip.sh
-if [ "$?" -ne "0" ]; then
-     echo "Build of zip files Failed"
-     exit 1
-fi
+REPO_HOME="${BASH_HOME}../"
+MODULE_HOME="${REPO_HOME}relaxng/modules/"
+rm "${MODULE_HOME}"*.zip >> /dev/null 2>&1
+cd "${MODULE_HOME}"
+zip -r9 ${MODULE_HOME}RNCModulesDeliberation1.02.zip *.rnc
