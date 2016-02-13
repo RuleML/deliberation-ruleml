bec21c1ac294eb1743e15364cdb8c171e1a1b035
greenTara <taraathan@gmail.com>
2016-02-13 10:06:54 -0500

    remove echo and irrelevant comments

--- bash/batch_rnc2simp.sh
+++ bash/batch_rnc2simp.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 # dc:rights [ 'Copyright 2015 RuleML Inc. -- Licensed under the RuleML Specification License, Version 1.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://ruleml.org/licensing/RSL1.0-RuleML. Disclaimer: THIS SPECIFICATION IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES, ..., EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. See the License for the specifics governing permissions and limitations under the License.' ]
 # Batch generate Simplified RNC from MYNG RNC
-# Prerequisites:
-#  Installation of curl
-# Prerequisites: batch_(web)config2rnc4simp.sh 
-# Dependencies:
-# rnc2simp.sh
-# relaxng/rnc4simp/*.rnc
 # This script will remove the contents of a sibling directory named /simplified.
-# Instructions:
-# run this script from the command line
 #
 # Notes
 # due to problems in Jing with simplification of qualified names as content, the explicit datatyping feature is not included.
@@ -24,7 +16,6 @@ rm "${SIMP_HOME}"*.rnc  >> /dev/null 2>&1
 for file in "${RNC4SIMP_HOME}"*.rnc
 do
   filename=$(basename "$file")
-  echo "${filename}"
   "${BASH_HOME}rnc2simp.sh" "$file" "${SIMP_HOME}${filename}"
   if [[ "$?" -ne "0" ]]; then
      echo "Simplification Failed for ${filename}"
