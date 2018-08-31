0a15bd403c75f783a72cbd137c3df928a529beb6
Tara Athan <taraathan@gmail.com>
2018-08-31 07:29:54 -0400

    Update schema_rnc.php

--- relaxng/schema_rnc.php
+++ relaxng/schema_rnc.php
@@ -1,5 +1,5 @@
 <?php
-// dc:rights [ 'Copyright 2015 RuleML Inc. - Licensed under the RuleML Specification License, Version 1.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://ruleml.org/licensing/RSL1.0-RuleML. Disclaimer: THIS SPECIFICATION IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES, ..., EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. See the License for the specifics governing permissions and limitations under the License.' ]    
+// dc:rights [ 'Copyright 2015-18 RuleML Inc. - Licensed under the RuleML Specification License, Version 1.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://ruleml.org/licensing/RSL1.0-RuleML. Disclaimer: THIS SPECIFICATION IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES, ..., EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. See the License for the specifics governing permissions and limitations under the License.' ]    
 //parse command line arguments into the $_GET variable
 //parse_str(implode('&', array_slice($argv, 1)), $_GET);
 ini_set('display_errors', 'On');
